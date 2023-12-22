03bbdd19b6d06f88952c19f34481cc82807455b8
mdabarik <mdabarik19@gmail.com>
2023-12-22 12:56:22 +0600

    origin added

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173']
+}));
 app.use(express.json());
 
 /************ MongoDB Connection ************/
